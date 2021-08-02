13393c0d9e1d583da0ab733620f9369796c88ede
Saulo Dias <saulo.dias@radixeng.com>
2021-08-01 23:32:04 -0300

    generaliza soma

--- calc.js
+++ calc.js
@@ -23,16 +23,21 @@ const valorSaqueConta = (saldo) => {
 const normalize = (value) => value.length < 3 
 
 const unmaskedValue = (id) => $(id).cleanVal()/100;
-const maskedValue = (value) => $('.money').masked(String(Math.round(value*100)).padStart(3,0).padEnd(3,0));
+const maskedValue = (value) => $(".money").masked(String(Math.round(value*100)).padStart(3,0).padEnd(3,0));
 
 const calc = (id) => {
-  const unmaskedSaldo = unmaskedValue('#saldo'+id);
+  const unmaskedSaldo = unmaskedValue("#saldo"+id);
   const { saqueValor } = valorSaqueConta(unmaskedSaldo);
   const maskedSaque = maskedValue(saqueValor);
   $("#saque"+id).val(maskedSaque);
 
-  const saldoTotal = unmaskedValue('#saldo1') + unmaskedValue('#saldo2') + unmaskedValue('#saldo3') + unmaskedValue('#saldo4') + unmaskedValue('#saldo5');
-  const saqueTotal = unmaskedValue('#saque1') + unmaskedValue('#saque2') + unmaskedValue('#saque3') + unmaskedValue('#saque4') + unmaskedValue('#saque5');
+  const saldos = Array.from($(".saldo"));
+  let saldoTotal = 0;
+  saldos.forEach(saldo => saldoTotal += unmaskedValue("#" + saldo.id));
+
+  const saques = Array.from($(".saque"));
+  let saqueTotal = 0;
+  saques.forEach(saque => saqueTotal += unmaskedValue("#" + saque.id));
 
   $("#saldoTotal").val(maskedValue(saldoTotal));
   $("#saqueTotal").val(maskedValue(saqueTotal));
